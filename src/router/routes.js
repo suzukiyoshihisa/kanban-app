38c91ba1c82695f6b30af56a9d685a8b88a5a0c0
鈴木良尚 <suzukiyoshihisa@hisap.local>
2020-09-03 17:13:30 +0900

    ルーティングの実装

---
+++ src/router/routes.js
@@ -0,0 +1,20 @@
+import KbnBoardView from '@/components/templates/KbnBoardView.vue'
+import KbnLoginView from '@/components/templates/KbnLoginView'
+import KbnTaskDetailModal from '@/components/templates/KbnTaskDetailModal'
+
+export default [{
+  path: '/',
+  component: KbnBoardView,
+  meta: {
+    requiresAuth: true
+  }
+}, {
+  path: '/',
+  component: KbnLoginView
+}, {
+  path: '/',
+  component: KbnTaskDetailModal,
+  meta: {
+    requiresAuth: true
+  }
+}]

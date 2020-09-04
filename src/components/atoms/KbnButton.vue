a08fb0f77e637fa0edf782076b5f70a4c812abec
鈴木良尚 <mf37459@gmail.com>
2020-09-05 01:06:41 +0900

    [add]vue file

---
+++ src/components/atoms/KbnButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    :class="classes"
+    :disabled="disabled"
+    type="button"
+    @click="handleClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "KbnButton",
+
+  props: {
+    type: {
+      type: String,
+      default: "button"
+    },
+    disabled: {}
+  }
+}
+</script>
+
+<style>
+</style>template

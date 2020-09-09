80a5cfbdcc80fa6abaf3c54003ccb222885925a1
鈴木良尚 <mf37459@gmail.com>
2020-09-09 19:35:37 +0900

    ログイン状態ビューの実装

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     "generator-star-spacing": "off",
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
     "import/no-webpack-loader-syntax":
       process.env.NODE_ENV === "production" ? "error" : "off"
   }
